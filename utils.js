ffd95d267bd575e980452a58ed3b3992455c2f07
dhananjay <dhananjaysingh1475@gmail.com>
2021-05-05 11:47:47 +0530

    added order api to get order details and placing order

--- utils.js
+++ utils.js
@@ -6,7 +6,24 @@ export const generateToken = (user) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin
-    },process.env.JWT_SECRET || 'olala' ,{
+    }, 'olala' ,{
         expiresIn: '30d',
     })
+}
+
+export const isAuth = (req,res,next ) => {
+    const authorization = req.headers.authorization;
+    if(authorization){
+        const token = authorization.slice(7,authorization.length); // Bearer XXXXX
+        jwt.verify(token, 'olala' , (err,decode)=>{
+            if(err){
+                res.status(401).send({message: 'Invalid Token'});
+            }else{
+                req.user = decode;
+                next();
+            }
+        })
+    }else{
+        res.status(401).send({message: 'No Token'});
+    }
 }
